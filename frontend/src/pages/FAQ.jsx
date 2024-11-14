fccc310f7fe85969eea7d5f2dbbac74dc72a7899
Bohdan Mahas <bohdanmahas.work@gmail.com>
2024-11-14 21:55:07 +0100

    [SPEN-12] Deleted background from FAQ page

--- frontend/src/pages/FAQ.jsx
+++ frontend/src/pages/FAQ.jsx
@@ -1,6 +1,5 @@
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
-import stylesHome from './Home.module.css'
 import stylesFAQ from './FAQ.module.css'
 import AccordionFAQ from '../components/AccordionFAQ'
 import { Button, Container, Typography } from '@mui/material'
@@ -10,7 +9,7 @@ import React, { useEffect } from 'react';
 function FAQ() {
   
   return (
-    <div className={stylesHome.home_container}>
+    <div>
       <Navbar />
       <div className={stylesFAQ.main_content}>
         <AccordionFAQ />
