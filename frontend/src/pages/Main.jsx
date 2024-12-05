2246bbbff1230c17f4762c7cdb85d7baf266f102
maxmoro3 <maxmoro2004@gmail.com>
2024-12-05 21:47:31 +0100

    [SPEN-34] Completed widget ''Total Balance''

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -1,11 +1,15 @@
+import BalanceWidget from "../components/BalanceWidget";
 import LoggedNavbar from "../components/LoggedNavBar";
+import styles from "./Main.module.css"
 
 function Main() {
   return (
     
     <div>
       <LoggedNavbar />
+      <BalanceWidget />
     </div>
+    
   );
 }   
 export default Main;
