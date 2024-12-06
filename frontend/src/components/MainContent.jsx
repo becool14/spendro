34c927aa9efe1b7e84ed09b95d990ecd0d29b477
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-12-07 00:23:41 +0100

    scroll_possible

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -1,27 +1,39 @@
 import styles from './MainContent.module.css'
-import { Button, Container, Typography } from '@mui/material'
+import { Button, Container, Typography, Box } from '@mui/material'
 
 function MainContent() {
   return (
     <div className={styles.main_content}>
+      
+      
       <Typography variant="h1" sx={{ fontSize: '2.125rem' }}>
         Spendro – Empower Your Finances,
-        <br /> Achieve Financial Freedom!
+        <br /> Achieve Financial Freedom! 
       </Typography>
+
       <Container>
         <Button
           style={{ backgroundColor: '#E6DEF5', color: '#5820BE' }}
-          variant="contained"
+         // variant="contained"
         >
           Learn More
         </Button>
         <Button
           style={{ backgroundColor: '#5820BE', color: '' }}
-          variant="contained"
+        //  variant="contained"
         >
           Get Started
         </Button>
+
+     
       </Container>
+
+ <h1>Welcome to Main Content</h1>
+      {[...Array(200)].map((_, index) => (
+        <p key={index}>This is line {index + 1}</p>
+      ))} 
+
+      
     </div>
   )
 }
