441a461d4b0c4345887300bc6c903b0ceb247cdb
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-12-07 01:29:49 +0100

    move topography down

--- frontend/src/components/MainContent.jsx
+++ frontend/src/components/MainContent.jsx
@@ -5,29 +5,38 @@ function MainContent() {
   return (
     <div className={styles.main_content}>
       
+
+      <Box sx={{
+        mt: '304px'
+      }}>
+        <Typography variant="h1" sx={{ fontSize: '2.125rem' }}>
+          Spendro – Empower 
+          <br />
+          Your Finances, Achieve 
+          <br />
+          Financial Freedom! 
+        </Typography>
+
+        <Container>
+          <Button
+            style={{ backgroundColor: '#E6DEF5', color: '#5820BE' }}
+          // variant="contained"
+          >
+            Learn More
+          </Button>
+          <Button
+            style={{ backgroundColor: '#5820BE', color: '' }}
+          //  variant="contained"
+          >
+            Get Started
+          </Button>
+
       
-      <Typography variant="h1" sx={{ fontSize: '2.125rem' }}>
-        Spendro – Empower Your Finances,
-        <br /> Achieve Financial Freedom! 
-      </Typography>
+        </Container>
 
-      <Container>
-        <Button
-          style={{ backgroundColor: '#E6DEF5', color: '#5820BE' }}
-         // variant="contained"
-        >
-          Learn More
-        </Button>
-        <Button
-          style={{ backgroundColor: '#5820BE', color: '' }}
-        //  variant="contained"
-        >
-          Get Started
-        </Button>
-
+      </Box>
+      
      
-      </Container>
-
  <h1>Welcome to Main Content</h1>
       {[...Array(200)].map((_, index) => (
         <p key={index}>This is line {index + 1}</p>
