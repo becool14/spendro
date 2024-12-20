205e2dd379605c58f3c77e3a4ec72d210cb70cba
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-12-20 19:48:06 +0100

    adding working buttons to navbar

--- frontend/src/components/FaqComponent.jsx
+++ frontend/src/components/FaqComponent.jsx
@@ -1,14 +1,182 @@
-import {Box, Typography} from '@mui/material';
-import styles from './FaqComponent.module.css'
+import { Box, Typography } from "@mui/material";
+import styles from "./FaqComponent.module.css";
 
-function FaqComponent(){
-    return(
-        <div className={styles.container}>
-            
-        </div>
-        
-
-    );
+function FaqComponent() {
+  return (
+    <div className={styles.container} id="faq">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce libero
+      orci, semper non egestas luctus, fringilla vitae nunc. Mauris sem nisl,
+      condimentum ut lectus nec, euismod auctor lacus. Fusce a neque quis purus
+      porta fermentum. Ut at diam tincidunt, feugiat augue eget, maximus eros.
+      Mauris ac quam sagittis, facilisis ligula vitae, lacinia lectus.
+      Pellentesque sed erat id lacus suscipit rutrum quis laoreet lacus.
+      Pellentesque habitant morbi tristique senectus et netus et malesuada fames
+      ac turpis egestas. Integer elementum arcu odio, ac vehicula massa dapibus
+      sed. Cras magna odio, dapibus et sodales ut, porttitor ac neque. Cras
+      dictum dolor imperdiet tincidunt tempor. Suspendisse euismod orci laoreet,
+      iaculis eros id, scelerisque est. Phasellus a sagittis metus. Nunc et
+      tristique leo. Fusce vitae pulvinar lectus, quis tristique nisl.
+      Vestibulum placerat aliquet tortor sed mollis. Suspendisse erat ante,
+      accumsan et scelerisque ut, malesuada hendrerit mauris. Vivamus quis
+      eleifend erat. Aliquam consectetur vitae libero congue semper. Sed vel
+      ullamcorper mi. Sed luctus lectus et mi auctor, accumsan molestie neque
+      volutpat. Nullam scelerisque nulla volutpat massa tincidunt, at tempor
+      lectus accumsan. Donec faucibus, nisl ac gravida lobortis, nibh augue
+      egestas sapien, et rhoncus justo lectus a massa. Etiam gravida rutrum
+      tellus ut ornare. In lobortis placerat erat. Suspendisse eu arcu vitae
+      elit rutrum lacinia in id sapien. Sed pretium lorem at quam porttitor
+      bibendum. Praesent eget dictum lacus. Maecenas at libero non orci
+      vestibulum luctus in quis lorem. Cras convallis lorem ut elit consequat
+      gravida id vitae quam. Nam faucibus tortor ut elit scelerisque tristique.
+      Nam nisi risus, vehicula vitae sapien at, egestas egestas arcu. Praesent
+      gravida mattis tellus nec fermentum. Vivamus condimentum, mauris sit amet
+      sagittis viverra, libero velit cursus dolor, in cursus mauris erat sed
+      enim. Suspendisse aliquam sapien arcu, in pharetra ex posuere at. Proin
+      pulvinar ultricies elit, in convallis diam. Suspendisse et justo nunc.
+      Cras ut mollis quam. Phasellus dapibus eget enim non consectetur. Fusce
+      non lectus id enim vehicula blandit at non lectus. Aliquam tempus in
+      ligula sed facilisis. Phasellus euismod orci finibus semper tempus.
+      Vestibulum nunc urna, hendrerit sed volutpat id, mattis in dolor. Quisque
+      ut dui id justo consectetur dignissim. Pellentesque tellus mi, porttitor
+      eget scelerisque sed, vulputate ut lectus. Quisque ornare risus libero, id
+      congue augue tincidunt eget. Fusce vitae risus velit. Nunc pulvinar
+      sollicitudin eros, ac dictum ipsum efficitur eu. Phasellus eget neque
+      egestas ipsum rhoncus gravida. Donec mollis purus eu nibh eleifend, a
+      interdum sem ornare. Duis tortor purus, vehicula sed ligula eu, egestas
+      ultrices purus. Nullam dignissim ultricies justo, non dictum risus dapibus
+      vel. Aliquam metus est, dignissim eu mollis eu, facilisis eu augue. Nunc
+      vitae dui nulla. Curabitur convallis lectus faucibus, ullamcorper mauris
+      vitae, ultrices ipsum. Nam iaculis dolor et cursus blandit. Nunc accumsan
+      lacus id lectus aliquet tempus. Aliquam tempus posuere quam, eu tristique
+      erat. Phasellus egestas ligula a dictum finibus. Proin et dictum quam.
+      Suspendisse vehicula enim ac pretium pellentesque. Nunc eu turpis
+      ultricies, tempor justo nec, lacinia nisl. Duis a viverra purus. Proin
+      porttitor enim at lectus vulputate efficitur. Vestibulum eget eros sapien.
+      Aenean imperdiet diam eros, at dapibus justo viverra et. Morbi et leo sed
+      nisi aliquam mattis. Aenean at justo libero. Interdum et malesuada fames
+      ac ante ipsum primis in faucibus. Pellentesque viverra, augue vel euismod
+      sagittis, risus ipsum fringilla nibh, at euismod nisi magna quis neque.
+      Maecenas blandit a urna eget vehicula. Nam tempus, ipsum non ultrices
+      malesuada, dui magna elementum ante, ac commodo dolor nunc sit amet sem.
+      Vestibulum venenatis magna eget faucibus finibus. Mauris lobortis mi
+      semper, sollicitudin mi sit amet, condimentum erat. Praesent dictum
+      convallis semper. Proin laoreet rhoncus ligula, sed gravida tortor
+      condimentum non. Donec blandit dolor ac massa lacinia, id lacinia justo
+      facilisis. Etiam bibendum, diam sit amet lacinia pulvinar, turpis tellus
+      blandit est, eget aliquam nulla massa ut arcu. Morbi ac dui vitae tellus
+      condimentum sodales ac a augue. Quisque lobortis dictum auctor. Donec
+      varius ante ex. Etiam augue leo, ullamcorper et venenatis quis, egestas
+      eget ligula. Proin sed elementum nulla. Maecenas accumsan convallis dui,
+      vel accumsan sapien scelerisque eu. Pellentesque accumsan, velit sit amet
+      convallis malesuada, dui nisi consectetur nibh, ut sodales felis enim eget
+      leo. Nunc vel odio ac sem consequat lobortis. Integer eget hendrerit
+      dolor. Curabitur orci odio, pulvinar quis mi id, eleifend gravida nibh.
+      Praesent facilisis nibh nec leo cursus, vel vehicula ante mollis. Integer
+      sit amet gravida tortor. Cras in leo erat. In in efficitur velit. Ut eu
+      scelerisque lectus. In consectetur, urna vel sollicitudin porta, sem neque
+      fringilla velit, eget luctus elit ante at dui. Aenean non nibh non justo
+      tincidunt egestas nec nec dui. Vivamus quis sollicitudin quam, quis mollis
+      est. Donec blandit, lorem et ultricies suscipit, tellus lorem volutpat
+      lacus, iaculis venenatis erat felis eget nulla. Nullam suscipit odio quis
+      enim facilisis auctor. Nullam vitae dolor nulla. Proin tempus vel orci sit
+      amet congue. Vestibulum ut sapien porttitor, tristique nisi ut, facilisis
+      tellus. Nunc vel nunc turpis. Nulla viverra erat non augue molestie, sit
+      amet bibendum tortor accumsan. Vestibulum vehicula faucibus orci, sit amet
+      semper odio congue ac. Mauris fermentum nunc eu risus pretium interdum.
+      Donec at felis vitae lectus tincidunt venenatis et vel sapien. Sed
+      suscipit ipsum id magna mollis, in convallis libero rhoncus. Nulla id
+      tellus sem. Praesent ornare libero nunc, quis viverra tellus egestas eu.
+      Proin eu ligula a metus placerat suscipit. Phasellus accumsan lobortis
+      suscipit. Nullam ultricies iaculis mauris, eu rutrum justo fringilla id.
+      Nulla ac scelerisque nisl. Phasellus pharetra rutrum magna vel vehicula.
+      Proin volutpat velit tortor, quis tempus ligula facilisis nec. Donec massa
+      mauris, ullamcorper mattis erat volutpat, cursus facilisis arcu. Donec vel
+      vulputate lacus, varius varius est. Sed non pulvinar lorem, nec egestas
+      libero. Vivamus mattis, metus vitae vulputate laoreet, mi odio tempus
+      neque, sit amet tincidunt felis sapien id leo. Aliquam ultricies quam
+      vitae nibh luctus suscipit. Etiam consequat, lectus sed pulvinar maximus,
+      ipsum urna maximus augue, et consectetur arcu felis volutpat mi. Morbi
+      faucibus ac ipsum at fringilla. Mauris vitae ex elementum, maximus nulla
+      luctus, varius neque. Nunc tincidunt congue neque. Sed non elit porttitor
+      metus aliquam maximus. Mauris dapibus congue efficitur. Vestibulum ante
+      ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; In
+      hac habitasse platea dictumst. Pellentesque vel sem ac augue consectetur
+      feugiat sed dictum dolor. Suspendisse risus purus, hendrerit at porttitor
+      eget, maximus et dolor. Integer mattis ultrices arcu sit amet volutpat.
+      Nam elit mauris, eleifend et eleifend sit amet, dictum a est. Fusce dui
+      magna, iaculis quis nunc vel, fermentum vestibulum enim. Vestibulum
+      condimentum maximus augue, id rhoncus orci blandit non. Donec elit lorem,
+      facilisis vitae accumsan at, consectetur nec enim. Nullam sagittis tortor
+      in dolor pretium luctus. Praesent mollis maximus enim vel eleifend. Proin
+      nec pharetra dui. Integer ut libero vitae nunc ultricies malesuada quis
+      quis massa. Mauris ut porttitor lacus, vel gravida lectus. Mauris vitae
+      condimentum ex, sit amet venenatis justo. Curabitur dolor ex, commodo
+      vitae maximus ut, dapibus gravida mi. Sed id elit tortor. Quisque
+      scelerisque neque eu pretium feugiat. Fusce id rhoncus lorem. Morbi nunc
+      justo, laoreet non odio sed, tempor congue enim. Fusce fringilla, purus id
+      ultricies faucibus, justo nibh sagittis tortor, in condimentum augue metus
+      at arcu. Mauris sit amet hendrerit felis. Duis dui turpis, suscipit vitae
+      metus vel, condimentum consequat dui. Nullam bibendum ac turpis a
+      tincidunt. Fusce ut malesuada ex. In posuere laoreet est, at vulputate
+      tortor feugiat a. Pellentesque pulvinar enim a sapien gravida porta. Proin
+      scelerisque sodales dolor, sit amet viverra turpis fringilla vel.
+      Suspendisse efficitur, urna vitae pretium iaculis, libero est scelerisque
+      urna, eu sagittis mi felis dictum nunc. Pellentesque habitant morbi
+      tristique senectus et netus et malesuada fames ac turpis egestas. Donec in
+      nulla faucibus, molestie massa et, scelerisque nisl. Vestibulum vestibulum
+      sem lorem, eget cursus libero euismod nec. Maecenas sodales, augue
+      convallis fermentum lobortis, velit nunc imperdiet elit, id aliquet orci
+      augue vel nulla. Duis quis augue a lacus pretium rutrum vel sed lorem. Nam
+      sodales ipsum facilisis ex congue scelerisque. Phasellus et pellentesque
+      nulla, non consectetur quam. Ut id sem felis. Phasellus mollis ullamcorper
+      dolor a volutpat. Maecenas eleifend venenatis tincidunt. Donec sit amet
+      neque nisl. Vestibulum ante ipsum primis in faucibus orci luctus et
+      ultrices posuere cubilia curae; Vestibulum ante ipsum primis in faucibus
+      orci luctus et ultrices posuere cubilia curae; Morbi tincidunt, urna sed
+      ultrices congue, turpis ipsum euismod tellus, sit amet interdum dui magna
+      vitae nunc. In condimentum accumsan odio, a euismod felis sodales quis.
+      Curabitur porta maximus lorem, non tristique felis egestas ut. Sed non
+      imperdiet felis, at volutpat tellus. Duis mattis, tellus ut tristique
+      tempor, mi nisl pretium diam, quis vulputate ipsum lorem nec ligula.
+      Phasellus hendrerit tortor in dapibus euismod. Ut nisi nulla, placerat
+      eget dolor vitae, aliquet dapibus tellus. Cras arcu felis, viverra sed
+      ligula in, lacinia tristique dui. Donec mattis id lorem non sollicitudin.
+      Quisque cursus lobortis congue. Cras faucibus nulla vitae ligula viverra,
+      sit amet tempus sapien efficitur. Praesent blandit mauris vel dui
+      fringilla, sed aliquam dolor vulputate. Praesent et consectetur lacus.
+      Donec sollicitudin scelerisque hendrerit. Vivamus ac molestie neque.
+      Maecenas commodo luctus ullamcorper. Suspendisse ut ex ullamcorper libero
+      tincidunt vestibulum. In semper sem sit amet urna dapibus mattis.
+      Pellentesque auctor nunc sit amet euismod ullamcorper. Nunc sodales augue
+      euismod metus posuere, nec volutpat diam dignissim. Morbi ullamcorper
+      nulla sit amet dui rutrum faucibus. Fusce fermentum egestas sodales. Nunc
+      elit mauris, sagittis id ipsum vestibulum, venenatis lobortis dolor.
+      Aenean dapibus a neque quis vehicula. Nullam efficitur lacus tortor, in
+      congue dui feugiat ac. Quisque nulla magna, consectetur id rutrum sed,
+      sodales vitae nunc. In hac habitasse platea dictumst. In hac habitasse
+      platea dictumst. Sed interdum massa enim, et mattis nunc tristique et.
+      Morbi a sem pharetra, varius urna vitae, condimentum est. Aliquam id
+      tristique ex. Nunc quam ex, commodo in magna facilisis, semper volutpat
+      ligula. Phasellus pretium, ipsum non congue aliquet, arcu lorem pulvinar
+      magna, quis bibendum dui velit sed lectus. Aliquam feugiat, mauris nec
+      porttitor pretium, enim leo volutpat erat, vel imperdiet tortor tortor eu
+      enim. Pellentesque aliquam mattis tellus, sed porta libero ullamcorper a.
+      Sed eget neque metus. Etiam imperdiet efficitur ipsum, non laoreet velit
+      vestibulum et. In gravida vulputate nisl sed venenatis. Nulla tristique
+      congue risus id dictum. Etiam placerat commodo fermentum. Nulla pharetra
+      maximus sapien, eget condimentum elit. Nullam consectetur dolor dolor, in
+      porta purus bibendum ut. Mauris commodo ligula at tempor volutpat. Duis
+      placerat, lorem quis scelerisque fermentum, felis urna mollis orci, vel
+      cursus enim erat ac sapien. Sed ut purus nec felis auctor vehicula. Donec
+      vitae congue augue. Quisque fringilla augue justo, at auctor nibh lacinia
+      in. In eget magna quis est condimentum hendrerit. Aliquam sit amet
+      sollicitudin massa. Pellentesque ut orci urna. Praesent vel aliquet magna,
+      sit amet luctus massa. In finibus eros vitae tempor mollis. Vestibulum
+      magna nibh, eleifend sit amet ultrices non, varius ac urna. Nullam
+      venenatis justo vel nulla malesuada tristique. Class aptent taciti
+      sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+    </div>
+  );
 }
 
 export default FaqComponent;
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,17 +1,17 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
-import LogoWhite from '../assets/icons/LogoWhite';
-import styles from './Navbar.module.css';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
+import LogoWhite from "../assets/icons/LogoWhite";
+import styles from "./Navbar.module.css";
 
 function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -24,17 +24,35 @@ function ResponsiveAppBar() {
     setAnchorElNav(null);
   };
 
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+  
+
   return (
-    <AppBar position="static" sx={{ bgcolor: 'black' }}>
+    <AppBar position="static" sx={{ bgcolor: "black" }}>
       <Container maxWidth="xl">
-        <Toolbar disableGutters sx={{ justifyContent: 'space-between', width: '100%' }}>
+        <Toolbar
+          disableGutters
+          sx={{ justifyContent: "space-between", width: "100%" }}
+        >
           {/* Логотип */}
-          <Link to="/" style={{ textDecoration: 'none' }}>
-            <LogoWhite/>
-          </Link>
+
+          {/* Navigation buttons */}
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <LogoWhite />
+            </Link>
+            <Button color="inherit" onClick={() => scrollToSection('about-us')}>about us</Button>
+            <Button color="inherit" onClick={() => scrollToSection('faq')}>FAQ</Button>
+            <Button color="inherit" onClick={() => scrollToSection('contact-us')}>contact us</Button>
+          </Box>
 
           {/* Респонсивна навігація */}
-          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
               aria-label="menu"
@@ -48,25 +66,31 @@ function ResponsiveAppBar() {
             <Menu
               id="menu-appbar"
               anchorEl={anchorElNav}
-              anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+              anchorOrigin={{ vertical: "top", horizontal: "right" }}
               keepMounted
-              transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+              transformOrigin={{ vertical: "top", horizontal: "right" }}
               open={Boolean(anchorElNav)}
               onClose={handleCloseNavMenu}
               sx={{
-                '& .MuiPaper-root': {
-                  backgroundColor: '#333', // Темний фон меню
-                  color: '#fff', // Білий текст
+                "& .MuiPaper-root": {
+                  backgroundColor: "#333", // Темний фон меню
+                  color: "#fff", // Білий текст
                 },
               }}
             >
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/login" style={{ textDecoration: 'none', color: 'primary' }}>
+                <Link
+                  to="/login"
+                  style={{ textDecoration: "none", color: "primary" }}
+                >
                   <Typography textAlign="center">login</Typography>
                 </Link>
               </MenuItem>
               <MenuItem onClick={handleCloseNavMenu}>
-                <Link to="/signup" style={{ textDecoration: 'none', color: 'primary' }}>
+                <Link
+                  to="/signup"
+                  style={{ textDecoration: "none", color: "primary" }}
+                >
                   <Typography textAlign="center">sign up</Typography>
                 </Link>
               </MenuItem>
@@ -74,11 +98,14 @@ function ResponsiveAppBar() {
           </Box>
 
           {/* Статичне меню для великих екранів */}
-          <Box sx={{ display: { xs: 'none', md: 'flex' }, gap: 2 }}>
-            <Button component={Link} to="/login" color="inherit">login</Button>
-            <Button component={Link} to="/signup" color="inherit">sign up</Button>
+          <Box sx={{ display: { xs: "none", md: "flex" }, gap: 2 }}>
+            <Button component={Link} to="/login" color="inherit">
+              login
+            </Button>
+            <Button component={Link} to="/signup" color="inherit">
+              sign up
+            </Button>
           </Box>
-
         </Toolbar>
       </Container>
     </AppBar>
