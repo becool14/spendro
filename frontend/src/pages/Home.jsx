130cb972c9a2fae00aac607502e29ff19aa81bde
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-12-20 10:43:16 +0100

    small changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,12 +2,16 @@ import Navbar from '../components/Navbar'
 import MainContent from '../components/MainContent'
 import Footer from '../components/Footer'
 import styles from './Home.module.css'
+import { Box } from '@mui/material'
 
 function Home() {
   return (
     <div className={styles.home_container}>
       <Navbar />
       <MainContent />
+      <Box>
+
+      </Box>
       <Footer />
     </div>
   )
