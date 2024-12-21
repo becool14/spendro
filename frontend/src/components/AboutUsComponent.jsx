70ba772d89a691f59ce2404b302162759c5a3775
SlawDEV <148549968+SlawDEV@users.noreply.github.com>
2024-12-21 01:19:44 +0100

    our mission

--- frontend/src/components/AboutUsComponent.jsx
+++ frontend/src/components/AboutUsComponent.jsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import styles from "./AboutUsComponent.module.css";
 import mainView from "../assets/pictures/main_view.png";
+import cards from "../assets/pictures/cards.png";
 
 function AboutUsComponent() {
   return (
     <div className={styles.container} id="about-us">
+      {/* about us section */}
       <Box
         sx={{
           display: "flex",
@@ -20,13 +22,62 @@ function AboutUsComponent() {
         >
           <img src={mainView} />
         </Box>
-        <Box sx={{  width: "35%", display:'flex', alignItems:'center'}}>
+        <Box sx={{ width: "35%", display: "flex", alignItems: "center" }}>
           <Typography>
-            [ O nas ] <br /> <br />Spendro powstało z myślą o ułatwieniu zarządzania
-            codziennymi wydatkami i budżetem domowym w prosty i intuicyjny
-            sposób. <br /> Naszą misją jest wspieranie ludzi w świadomym
-            podejściu do finansów osobistych, aby każdy mógł osiągać swoje cele
-            finansowe i lepiej planować przyszłość.
+            [ O nas ] <br /> <br />
+            Spendro powstało z myślą o ułatwieniu zarządzania codziennymi
+            wydatkami i budżetem domowym w prosty i intuicyjny sposób. <br />{" "}
+            Naszą misją jest wspieranie ludzi w świadomym podejściu do finansów
+            osobistych, aby każdy mógł osiągać swoje cele finansowe i lepiej
+            planować przyszłość.
+          </Typography>
+        </Box>
+      </Box>
+
+      {/* our mission section */}
+      <Box
+        sx={{
+          bgcolor: "#5820BE",
+          width: "95%",
+          margin: "0 auto",
+          display: "flex",
+          flexDirection: "column",
+          padding: "10px 90px 30px 90px",
+          gap: "20px",
+          borderRadius: "20px",
+          mt:'70px'
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <img src={cards} width="100%" />
+        </Box>
+        <Box>
+          <Typography>[ Nasza misja ]</Typography>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            gap: "10%",
+          }}
+        >
+          <Typography sx={{ width: "45%" }}>
+            Wierzymy, że dobre zarządzanie finansami osobistymi zaczyna się od
+            jasnego obrazu tego, na co wydajemy nasze pieniądze. Spendro
+            umożliwia szybkie i wygodne monitorowanie wydatków oraz pomaga
+            użytkownikom zrozumieć ich nawyki finansowe.
+          </Typography>
+
+          <Typography sx={{ width: "45%" }}>
+            Dążymy do tego, aby nasza aplikacja była czymś więcej niż tylko
+            narzędziem – chcemy, aby była wsparciem na drodze do poprawy
+            kondycji finansowej i realizacji długoterminowych planów.
           </Typography>
         </Box>
       </Box>
